2187fc0961684014f299b177cf4deed7a5d05ad5
chiroruxx <dormir.kazu@gmail.com>
2021-03-03 20:10:17 +0900

    change route

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/user', MigrationController::class);
+    Route::get('migrate', MigrationController::class);
 });

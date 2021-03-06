d386d1738b0b8851247917202b4bbc59b8bc7126
chiroruxx <dormir.kazu@gmail.com>
2021-03-06 17:07:52 +0900

    Add Hatena XML.

--- resources/views/api/hatena/create.blade.php
+++ resources/views/api/hatena/create.blade.php
@@ -4,7 +4,7 @@
  */
 @endphp
 
-{!! '<?xml version="1.0" encoding="utf-8"?>' !!}
+{{ '<?xml version="1.0" encoding="utf-8"?>' }}
 <entry xmlns="http://www.w3.org/2005/Atom"
        xmlns:app="http://www.w3.org/2007/app">
     <title>{{ $post->getTitle() }}</title>
